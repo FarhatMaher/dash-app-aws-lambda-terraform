7c5e339b10e45a3ac65ae37ae250c0939e97d7f4
maher-rddi <maher@edflabs.net>
2025-01-24 15:17:42 +0000

    Add docker_host variable to Terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,4 +8,8 @@ variable "environment" {
   description = "The environment"
   default     = "dev"
 }
-
+variable "docker_host" {
+  type        = string
+  description = "The docker host"
+  default     = "unix:///var/run/docker.sock"
+}
