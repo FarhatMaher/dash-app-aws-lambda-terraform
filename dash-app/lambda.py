2e745f6858879beb7660285751c34f464ffe4f8e
maher-rddi <maher@edflabs.net>
2025-01-25 09:59:39 +0000

    Update Lambda handler function name in Dash app
    - Rename lambda_handler to handler in the Lambda function
    - Maintain consistent function naming convention

--- dash-app/lambda.py
+++ dash-app/lambda.py
@@ -1,5 +1,5 @@
 from app import app
 import awsgi2
 # Lambda handler, Do not remove or change this
-def lambda_handler(event, context):
+def handler(event, context):
     return awsgi2.response(app.server, event, context)
