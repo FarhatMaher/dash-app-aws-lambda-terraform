c791f53e02d34960c11b0da718ce415a810950be
maher-rddi <maher@edflabs.net>
2025-01-26 11:49:52 +0000

    Remove suppression of callback exceptions in Dash app configuration

--- dash-app/app.py
+++ dash-app/app.py
@@ -5,8 +5,6 @@ REQUESTS_PATHNAME_PREFIX = os.environ.get('REQUESTS_PATHNAME_PREFIX')
 
 app = Dash(__name__, compress=False, requests_pathname_prefix=REQUESTS_PATHNAME_PREFIX)
 app.title = "Dash on Lambda AWS."
-app.config.suppress_callback_exceptions = True
-
 app.layout = html.Div([
     html.H1('Dash'),
     html.Div('A web application framework for Python.'),
